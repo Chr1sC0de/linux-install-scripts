888495703ed619edb149f01d2873ced9f8c36bf0
Chris Mamon <chrisam1993@gmail.com>
2024-08-17 19:45:20 +1000

    feature: refactored

--- uninstall-astral.sh
+++ uninstall-astral.sh
@@ -4,33 +4,39 @@
 uninstall astral environment
 '
 
-decorated_uninstall() {
-    APPLICATION=$1
-    UNINSTALL_METHOD=$2
-    echo "INFO: $APPLICATION installation found, uninstalling"
-    eval "$UNINSTALL_METHOD"
-    echo "INFO: $APPLICATION finished uninstalling"
-}
-
 echo "INFO: uninstalling astral environment"
 
-uninstall_uv() {
-    UV_LOCATION="$HOME/.cargo/bin/uv"
-    UVX_LOCATION="$HOME/.cargo/bin/uvx"
-    LOCATIONS=("$UV_LOCATION" "$UVX_LOCATION")
+uninstall_all() {
+    FILES=(
+        "$HOME/.cargo/bin/uv"
+        "$HOME/.cargo/bin/uvx"
+    )
+    DIRECTORIES=(
+        "$HOME/.rye"
+    )
 
-    for APP_LOCATION in "${LOCATIONS[@]}"; do
-        if [[ -f "$APP_LOCATION" ]]; then
-            echo "INFO: $APP_LOCATION found, removing"
-            rm "$APP_LOCATION"
-            echo "INFO: finished removing $APP_LOCATION"
+    for FILE in "${FILES[@]}"; do
+        if [[ -f "$FILE" ]]; then
+            echo "INFO: $FILE found, removing"
+            rm "$FILE"
+            echo "INFO: finished removing $FILE"
         else
-            echo "INFO: $APP_LOCATION not found"
+            echo "INFO: $FILE not found"
         fi
+    done
 
+    for DIRECTORY in "${DIRECTORIES[@]}"; do
+        if [[ -d "$DIRECTORY" ]]; then
+            echo "INFO: $DIRECTORY found, removing"
+            rm "$DIRECTORY" -rf
+            echo "INFO: finished removing $DIRECTORY"
+        else
+            echo "INFO: $DIRECTORY not found"
+        fi
     done
 
 }
 
-decorated_uninstall "rye" "rye uninstall"
-decorated_uninstall "uv" "uninstall_uv"
+uninstall_all
+
+echo "INFO: fininshed uninstalling astral environment, remember to remove . '$HOME/.rye/env' from profile"
