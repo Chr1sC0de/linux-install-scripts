30c1110442a0b435a19683f0e3b009d6823b9da3
Chris Mamon <chrisam1993@gmail.com>
2024-08-17 19:52:45 +1000

    feature: now remove autocomplte

--- uninstall-astral.sh
+++ uninstall-astral.sh
@@ -6,37 +6,34 @@ uninstall astral environment
 
 echo "INFO: uninstalling astral environment"
 
-uninstall_all() {
-    FILES=(
-        "$HOME/.cargo/bin/uv"
-        "$HOME/.cargo/bin/uvx"
-    )
-    DIRECTORIES=(
-        "$HOME/.rye"
-    )
+FILES=(
+    "$HOME/.cargo/bin/uv"
+    "$HOME/.cargo/bin/uvx"
+)
+DIRECTORIES=(
+    "$HOME/.rye"
+)
 
-    for FILE in "${FILES[@]}"; do
-        if [[ -f "$FILE" ]]; then
-            echo "INFO: $FILE found, removing"
-            rm "$FILE"
-            echo "INFO: finished removing $FILE"
-        else
-            echo "INFO: $FILE not found"
-        fi
-    done
+for FILE in "${FILES[@]}"; do
+    if [[ -f "$FILE" ]]; then
+        echo "INFO: $FILE found, removing"
+        rm "$FILE"
+        echo "INFO: finished removing $FILE"
+    else
+        echo "INFO: $FILE not found"
+    fi
+done
 
-    for DIRECTORY in "${DIRECTORIES[@]}"; do
-        if [[ -d "$DIRECTORY" ]]; then
-            echo "INFO: $DIRECTORY found, removing"
-            rm "$DIRECTORY" -rf
-            echo "INFO: finished removing $DIRECTORY"
-        else
-            echo "INFO: $DIRECTORY not found"
-        fi
-    done
+for DIRECTORY in "${DIRECTORIES[@]}"; do
+    if [[ -d "$DIRECTORY" ]]; then
+        echo "INFO: $DIRECTORY found, removing"
+        rm "$DIRECTORY" -rf
+        echo "INFO: finished removing $DIRECTORY"
+    else
+        echo "INFO: $DIRECTORY not found"
+    fi
+done
 
-}
-
-uninstall_all
+rm "$HOME/.local/share/bash-completion/completions/rye.bash"
 
 echo "INFO: fininshed uninstalling astral environment, remember to remove . '$HOME/.rye/env' from profile"
