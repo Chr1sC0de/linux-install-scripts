e5d6678767cff3bff88f1042554f8b20cf3e5967
Chris Mamon <chrisam1993@gmail.com>
2024-08-18 19:58:00 +1000

    feature: updated bat install script to include symlink

--- install-bat.sh
+++ install-bat.sh
@@ -1,1 +1,22 @@
-apt install bat
+#!/bin/bash
+
+SCRIPT_DIR="$(dirname -- "${BASH_SOURCE[@]}")"
+
+source "$SCRIPT_DIR/utils.sh"
+
+if ! type bat &>/dev/null; then
+    echoinfo "Installing bat"
+    if sudo apt install bat; then
+        echoinfo "Finished installing batcat"
+    else
+        echoinfo "Failed to install bat"
+        exit 1
+    fi
+    echoinfo "Creating symbolic link from batcat -> bat"
+    mkdir -p "$HOME/.local/bin"
+    ln -s "/usr/bin/batcat" "$HOME/.local/bin/bat"
+    echoinfo "Finished creating symbolic link from batcat -> bat"
+    echoinfo "Finished installing bat"
+else
+    echoinfo "bat found, skipping"
+fi
