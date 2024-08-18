21003fcc1a94c1d7332b6d10a0f14847ebeec352
Chris Mamon <chrisam1993@gmail.com>
2024-08-18 20:04:46 +1000

    feature: added fzf uninstall

---
+++ uninstall-fzf.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(dirname -- "${BASH_SOURCE[@]}")"
+
+source "$SCRIPT_DIR/utils.sh"
+
+if type fzf &>/dev/null; then
+    echoinfo "fzf command found, uninstalling"
+    "$HOME/.fzf/uninstall"
+    rm "$HOME/.fzf" -rf
+    echoinfo "Finished uninstall fzf"
+else
+    echoinfo "fzf not found, skip uninstalling"
+fi
