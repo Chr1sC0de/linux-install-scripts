747f130ed261659a6b6d81fdaf38c929aa2e11b6
Chris Mamon <chrisam1993@gmail.com>
2024-08-18 17:49:30 +1000

    feature: updated to use echoinfo

--- install-aws-cli.sh
+++ install-aws-cli.sh
@@ -5,16 +5,16 @@ install the aws cli
 '
 
 if ! type aws &>/dev/null; then
-    echo "INFO: aws cli not installed, installing"
+    echoinfo "aws cli not installed, installing"
     curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
     unzip awscliv2.zip
     sudo ./aws/install
 
-    echo "INFO: cleaning remnants"
+    echoinfo "Cleaning remnants"
     rm -rf aws
     rm awscliv2.zip
-    echo "INFO: finished cleaning remnants"
-    echo "INFO: finished installing aws cli"
+    echoinfo "Finished cleaning remnants"
+    echoinfo "Finished installing aws cli"
 else
-    echo "INFO: aws cli already installed"
+    echoinfo "aws cli already installed"
 fi
--- uninstall-aws-cli.sh
+++ uninstall-aws-cli.sh
@@ -7,22 +7,22 @@ uninstall the aws cli binaries
 AWS_BINARIES=("/usr/local/bin/aws" "/usr/local/bin/aws_completer")
 AWS_CLI_FOLDER="/usr/local/aws-cli"
 
-echo "INFO: uninstalling aws cli"
+echoinfo "Uninstalling aws cli"
 
 for FILE in "${AWS_BINARIES[@]}"; do
     if [[ -f $FILE ]]; then
-        echo "INFO: found $FILE, removing"
+        echoinfo "Found $FILE, removing"
         rm "$FILE"
-        echo "INFO: finished removing $FILE"
+        echoinfo "Finished removing $FILE"
     else
-        echo "INFO: $FILE not found"
+        echoinfo "$FILE not found"
     fi
 done
 
 if [[ -d $AWS_CLI_FOLDER ]]; then
-    echo "INFO: found $AWS_CLI_FOLDER, removing"
+    echoinfo "Found $AWS_CLI_FOLDER, removing"
     rm $AWS_CLI_FOLDER -rf
-    echo "INFO: finished removing $AWS_CLI_FOLDER"
+    echoinfo "finished removing $AWS_CLI_FOLDER"
 fi
 
-echo "INFO: finished installing aws cli"
+echoinfo "Finished installing aws cli"
