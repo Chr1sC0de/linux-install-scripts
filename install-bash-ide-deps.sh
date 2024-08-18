e483279bf7ea5319fb656f1a4a8973a7a557ffa9
Chris Mamon <chrisam1993@gmail.com>
2024-08-18 18:05:11 +1000

    feature: only need to install shellcheck if vscode is installed, otherwise use mason

--- install-bash-ide-deps.sh
+++ install-bash-ide-deps.sh
@@ -4,4 +4,18 @@
 dependencies for bash ide language server
 '
 
-sudo apt install shellcheck
+SCRIPT_DIR="$(dirname -- "${BASH_SOURCE[0]}")"
+
+source "$SCRIPT_DIR/utils.sh"
+
+if type code &>/dev/null; then
+    echoinfo "VSCode found installing shellcheck for bash ide"
+    apt install shellcheck
+    if [[ $? -eq 0 ]]; then
+        echoinfo "Finished installing shellcheck"
+    else
+        echoinfo "Failed to install shellcheck"
+    fi
+else
+    echoinfo "VSCode not found skipping installation of shellcheck, install shellcheck from Mason for neovim"
+fi
