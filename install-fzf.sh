b08f53e24bfc7e1eb15b20ab441919f43ea1138b
Chris Mamon <chrisam1993@gmail.com>
2024-08-18 20:04:04 +1000

    feature: added fzf install

--- install-fzf.sh
+++ install-fzf.sh
@@ -1,3 +1,14 @@
-#! /bin/bash
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+#!/bin/bash
+
+SCRIPT_DIR="$(dirname -- "${BASH_SOURCE[@]}")"
+
+source "$SCRIPT_DIR/utils.sh"
+
+if ! type fzf &>/dev/null; then
+    echoinfo "fzf command not found installing"
+    git clone --depth 1 https://github.com/junegunn/fzf.git "$HOME/.fzf"
+    "$HOME/.fzf/install"
+    echoinfo "Finished installing fzf"
+else
+    echoinfo "fzf found, skipping"
+fi
